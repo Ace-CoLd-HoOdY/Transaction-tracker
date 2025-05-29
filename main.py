2c5248790d476cdca6665d0475b36e71d8c1a66a
Ace-CoLd-HoOdY <max.gongalo@gmail.com>
2025-05-29 13:50:20 +0300

    Finished

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 transactions = []
 
 def show_menu():
+    print()
     print("1. Add income")
     print("2. Add expense")
     print("3. List  transactions")
     print("4. Show balance")
     print("5. Exit")
+    print()
 
 def add_income():
     s = int(input("Enter the amount of income: "))
@@ -37,20 +39,38 @@ def list_transactions():
             else:
                 print("Income", i)
 
-
-
-show_menu()
-choice = int(input("Choose what you want: "))
+def show_balance():
+    if len(transactions) == 0:
+        total = 0
+    else:
+        total = sum(transactions)
+    
+    print(f"Your balance: {total}")
 
 while True:
+    show_menu()
+    choice = int(input("Choose what you want: "))
     if choice == 1:
+        print()
         add_income()
+        print()
     elif choice == 2:
+        print()
         add_expense()
+        print()
     elif choice == 3:
+        print()
         list_transactions()
+        print()
     elif choice == 4:
-        print("not now")
+        print()
+        show_balance()
+        print()
     elif choice == 5:
+        print()
         print("Get out!")
         break
+    else:
+        print()
+        print("Wrong choice")
+        print()
